00eac78889fb53fae9266bc0d2cfe3b3c6564555
K. Adam White <kadamwhite@users.noreply.github.com>
2017-07-14 15:44:15 -0400

    Add initial app

---
+++ src/components/Jobs.vue
@@ -0,0 +1,58 @@
+<template>
+  <ul class="jobs">
+    <li v-for="job in jobs">
+      {{ job }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import licenses from '../data/licenses';
+
+// eslint-disable-next-line
+console.log(licenses);
+const jobs = [
+  'White Mage',
+  'Uhlan',
+  'Machinist',
+  'Red Battlemage',
+  'Knight',
+  'Monk',
+  'Time Battlemage',
+  'Foebreaker',
+  'Archer',
+  'Black Mage',
+  'Bushi',
+  'Shikari',
+];
+
+export default {
+  name: 'jobs',
+  data() {
+    return {
+      jobs,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
